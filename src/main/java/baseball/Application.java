6051d9c53b27ba13c1c77c38d0acc807eb1d3d5e
June <tkdwns159@naver.com>
2021-11-27 19:48:36 +0900

    style: add whitespace and blank lines

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,21 +3,23 @@ package baseball;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static class Game {
-        public List<Integer> answer;
-        public boolean done;
-        Game() {
+	public static class Game {
+		public List<Integer> answer;
+		public boolean done;
+
+		Game() {
 			startGame();
-            done = false;
+			done = false;
 		}
 
-		private void startGame(){
+		private void startGame() {
 			answer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
 		}
-    }
 
-    public static void main(String[] args) {}
+	}
+
+	public static void main(String[] args) {
+	}
 }
