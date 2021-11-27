2f5d47bd799c84f5877ade859de8b1eff32621b3
June <tkdwns159@naver.com>
2021-11-27 19:03:41 +0900

    feat: Game class
    Defined Game class :
    - class variables
    - startGame
    - constructor

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+    public static class Game {
+        public List<Integer> answer;
+        public boolean done;
+        Game() {
+			startGame();
+            done = false;
+		}
+
+		private void startGame(){
+			answer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+		}
     }
+
+    public static void main(String[] args) {}
 }
