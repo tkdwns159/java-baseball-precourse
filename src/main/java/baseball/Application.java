11c2e787de71799610a43eb474507eada8609f13
June <tkdwns159@naver.com>
2021-11-27 21:48:09 +0900

    feat: add validateNumbers and a couple of changes
    Added following to Game class:
    - validateNumbers function
    
    Deleted from Game class:
    - answer argument of compareEach
    
    Changed type of pos argument at compareEach:
    from Integer to int
    
    'answer' is directly accessible since it is class variable.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
@@ -18,7 +19,7 @@ public class Application {
 			answer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
 		}
 
-		public int compareEach(List<Integer> answer, Integer num, int pos) {
+		public int compareEach(int num, int pos) {
 			if (answer.get(pos).equals(num)) {
 				return 1;
 			}
@@ -28,6 +29,37 @@ public class Application {
 				}
 			}
 			return -1;
+		}
+
+		public void validateNumbers() {
+			String input = Console.readLine();
+			int[] output = new int[] {0, 0};
+			for (int i = 0; i < 3; i++) {
+				int number = input.charAt(i) - '0';
+				int res = compareEach(number, i);
+				if (res > -1) {
+					output[res] += 1;
+				}
+			}
+			if (output[1] == 3) {
+				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+				done = true;
+			} else if (output[0] == 3) {
+				System.out.println("낫싱");
+			} else {
+				String ballMsg = "";
+				String strikeMsg = "";
+				if (output[0] > 0) {
+					ballMsg += String.format("%d볼", output[0]);
+				}
+				if (output[1] > 0) {
+					strikeMsg += String.format("%d스트라이크", output[1]);
+				}
+				if (!ballMsg.equals("")) {
+					ballMsg += " ";
+				}
+				System.out.println(ballMsg + strikeMsg);
+			}
 		}
 
 	}
